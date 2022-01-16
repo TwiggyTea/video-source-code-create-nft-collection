fdb359d88b4a17d7836aa5ee53ae6e050a740cfc
TwiggyTea <81997030+TwiggyTea@users.noreply.github.com>
2022-01-16 05:16:20 -0500

    Functioning through IPFS upload

--- src/config.js
+++ src/config.js
@@ -26,13 +26,17 @@ const layerConfigurations = [
   {
     growEditionSizeTo: 5,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "backgroundColor" },
+      { name: "squareOne" },
+      { name: "squareTwo" },
+      { name: "squareThree" },
+      { name: "squareFour" },
+      { name: "squareFive" },
+      { name: "squareSix" },
+      { name: "squareSeven" },
+      { name: "squareEight" },
+      { name: "squareNine" },
+      { name: "squareTen" }
     ],
   },
 ];
