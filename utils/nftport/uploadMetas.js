3306e94a7d26f7d97d9aa7b92d99ec8c1f172416
TwiggyTea <81997030+TwiggyTea@users.noreply.github.com>
2022-01-16 06:18:13 -0500

    auth added to uploadMetas

--- utils/nftport/uploadMetas.js
+++ utils/nftport/uploadMetas.js
@@ -4,7 +4,7 @@ const basePath = process.cwd();
 const fs = require("fs");
 const readDir = `${basePath}/build/json`; // change this directory if you are uploading generic images first in order to do a reveal.
 
-const AUTH = 'YOUR API KEY HERE';
+const AUTH = process.env.API_KEY;
 const TIMEOUT = 1000; // Milliseconds. Extend this if needed to wait for each upload. 1000 = 1 second.
 
 const allMetadata = [];
